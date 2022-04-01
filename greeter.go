ca17188d77ef4f54bd0fa2c3a9a6a8a2c9391d6b
KiefeEd <kiefe.edward@gmail.com>
2022-04-01 21:19:29 +0700

    Added Greet() with time phase of the day.
    Hello, just more advanced.

--- greeter.go
+++ greeter.go
@@ -7,3 +7,19 @@ import(
 func Hello(user string) {
 	fmt.Printf("Hello, %s!", user)
 }
+
+func Greet(user string) {
+	var timenow = time.Now().UTC().Hour() + 7 //GMT+7
+	phase := "Midnight"
+	if timenow < 6 || timenow > 22{
+		phase = "Night"
+	} else if timenow >= 6 && timenow < 12 {
+		phase = "Morning"
+	} else if timenow >= 12 && timenow < 17 {
+		phase = "Afternoon"
+	} else {
+		phase = "Evening"
+	}
+		fmt.Printf("Good %s, %s!", phase, user)
+	
+}
