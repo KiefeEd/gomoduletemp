022e0c5745d236b3f0064962fa8d0428e7019d0a
KiefeEd <kiefe.edward@gmail.com>
2022-04-01 21:27:45 +0700

    Minor addition for Greet()
     added timezone parameter based on UTC.

--- greeter.go
+++ greeter.go
@@ -8,8 +8,11 @@ func Hello(user string) {
 	fmt.Printf("Hello, %s!", user)
 }
 
-func Greet(user string) {
-	var timenow = time.Now().UTC().Hour() + 7 //GMT+7
+func Greet(user string, fromutc int8) {
+	if fromutc < -11 || fromutc > 13 {
+		fmt.Println("Invalid Timezone from UTC provided. Example input: 7")
+	} else {
+	var timenow = time.Now().UTC().Hour() + fromutc //GMT+7
 	phase := "Midnight"
 	if timenow < 6 || timenow > 22{
 		phase = "Night"
@@ -21,5 +24,5 @@ func Greet(user string) {
 		phase = "Evening"
 	}
 		fmt.Printf("Good %s, %s!", phase, user)
-	
+	}	
 }
